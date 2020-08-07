3ab0ce83b2012c296d6100a8167d6ced97d5dfdc
steveo <steveoverall@hotmail.co.uk>
2020-08-07 20:43:08 +0100

    * Split both standard and target config sets into separate red and blue arms
    * Replaced the lambda_line_red/blue etc with just lambda_high/low as now a set only has red or blue spectra
    
    * Improved the usability of the exported CSS_SPEC fits file. Reused the PrimaryHDU from the _1_ source fits and based the CSS header on the first available SS header from the source fits.
    
    * Made a start on revising the sky subtraction config fo the the target red spectra.

--- data/LT/sky_subtraction_pipeline.py
+++ data/LT/sky_subtraction_pipeline.py
@@ -21,15 +21,17 @@ def read_setting(group, name, default=None, printout=True):
 
 
 settings = json.load(open("spec_calibration.json"))
-for spec_set in ["target_observation-red", "target_observation-blue", "standard_observation-Hilt102"]:
+for spec_set in ["std-obs-Hilt102-blue", "std-obs-Hilt102-red", "target-obs-blue", "target-obs-red"]:
     spec_set_settings = settings["sky_subtraction"][spec_set]
-    lambda_h_alpha = read_setting(spec_set_settings, "lambda_line_red", 6563)
-    lambda_cont_red = read_setting(spec_set_settings, "lambda_cont_red", 6000)
-    lambda_h_beta = read_setting(spec_set_settings, "lambda_line_blue", 4861)
-    lambda_cont_blue = read_setting(spec_set_settings, "lambda_cont_blue", 4000)
+    lambda_high = read_setting(spec_set_settings, "lambda_high", 6563)
+    lambda_low = read_setting(spec_set_settings, "lambda_low", 6000)
     lambda_delta = read_setting(spec_set_settings, "lambda_delta", 1000e3)
+    high_region = Spectrum1DEx.spectral_region_centred_on(lambda_high, lambda_delta)
+    low_region = Spectrum1DEx.spectral_region_centred_on(lambda_low, lambda_delta)
+
     plot_rss_spectra = read_setting(spec_set_settings, "plot_rss_spectra", True)
     expand_rss_spectra = read_setting(spec_set_settings, "expand_rss_spectra", False)
+
     # These are defaults which may be overridden on a fibre by fibre basis.
     def_sky_th = read_setting(spec_set_settings, "sky_th", 3)
     def_sky_spike_th = read_setting(spec_set_settings, "sky_spike_th", 100)
@@ -44,28 +46,30 @@ for spec_set in ["target_observation-red", "target_observation-blue", "standard_
         print(f"\n\nProcessing fits group: {fits_group}\n--------------------------------------")
         fits_group_settings = spec_set_settings["subtraction_settings"][fits_group]
         fits_group_ss_spectra = SpectrumList()
-        header1 = None
+
+        # We'll note these are we got through the group as these will form the bases of the output fits.
+        source_fits_full_name1 = None
+        spec_ss_header = None
 
         for fits_file_name in spec_set_settings["subtraction_settings"][fits_group]:
             print(f"* Processing {spec_set} / subtraction_settings / {fits_group} / {fits_file_name}")
             fits_settings = fits_group_settings[fits_file_name]
 
-            non_ss_spectra, header = FrodoSpecDS.read_spectra(
-                source_dir / fits_file_name, "RSS_NONSS", None, True, fits_file_name)
-            is_blue = "blue" in str.lower(header["CONFNAME"])
+            source_fits_full_name = source_dir / fits_file_name
+            non_ss_spectra, hdr = FrodoSpecDS.read_spectra(source_fits_full_name, "RSS_NONSS", None, True, fits_file_name)
+            is_blue = "blue" in str.lower(hdr["CONFNAME"])
+            if source_fits_full_name1 is None:
+                source_fits_full_name1 = source_fits_full_name
 
             # For each NON-SS spectra we calculate the flux ratio between chosen high and low flux regions
             # The two ranges are chosen specifically to discriminate between an object and a "sky" spectrum
-            peak_region = Spectrum1DEx.spectral_region_centred_on(lambda_h_beta if is_blue else lambda_h_alpha, lambda_delta)
-            cont_region = Spectrum1DEx.spectral_region_centred_on(lambda_cont_blue if is_blue else lambda_cont_red, lambda_delta)
             num_spectra = non_ss_spectra.shape[0]
             flux_ratios = np.zeros(num_spectra)
             for spec_ix in np.arange(0, num_spectra):
-                # Don't use specutils.extract_spectrum here as it's incredibly slow.
                 wl_set = non_ss_spectra.wavelength[spec_ix, :]
                 non_ss_spectrum = non_ss_spectra.flux[spec_ix, :]
-                h_wavelength_sel = (wl_set >= peak_region.lower) & (wl_set <= peak_region.upper)
-                c_wavelength_sel = (wl_set >= cont_region.lower) & (wl_set <= cont_region.upper)
+                h_wavelength_sel = (wl_set >= high_region.lower) & (wl_set <= high_region.upper)
+                c_wavelength_sel = (wl_set >= low_region.lower) & (wl_set <= low_region.upper)
                 flux_ratios[spec_ix] = sum(non_ss_spectrum[h_wavelength_sel]) / sum(non_ss_spectrum[c_wavelength_sel])
             min_flux_ratio = min(flux_ratios)
             max_flux_ratio = max(flux_ratios)
@@ -79,11 +83,11 @@ for spec_set in ["target_observation-red", "target_observation-blue", "standard_
             plotting.plot_histogram_to_ax(fig.add_subplot(gs[0, 0]), flux_ratios, is_blue)
             plotting.plot_fibre_heatmap_to_ax(fig, fig.add_subplot(gs[0, 1]), flux_ratios)
             try:
-                ss_spec, _header = FrodoSpecDS.read_spectrum(source_dir / fits_file_name, "SPEC_SS", header=True)
-                if header1 is None:
-                    header1 = _header
+                ss_spec, header = FrodoSpecDS.read_spectrum(source_fits_full_name, "SPEC_SS", header=True)
                 plotting.plot_spectrum_to_ax(fig.add_subplot(gs[1, :]), ss_spec,
-                                             "Standard pipeline sky subtracted spectrum", cont_region, peak_region)
+                                             "Standard pipeline sky subtracted spectrum", low_region, high_region)
+                if spec_ss_header is None:
+                    spec_ss_header = header
             except KeyError:
                 print("Missing SPEC_SS data")
 
@@ -137,14 +141,14 @@ for spec_set in ["target_observation-red", "target_observation-blue", "standard_
             ss_obj_flux = np.subtract(avg_obj_flux, avg_sky_flux)
             ss_spectrum = Spectrum1DEx(flux=ss_obj_flux, spectral_axis=non_ss_spectra.spectral_axis[0])
             plotting.plot_spectrum_to_ax(fig.add_subplot(gs[2, :]), ss_spectrum, "My pipeline sky subtracted spectrum",
-                                         sky_flux=avg_sky_flux, c_range=cont_region, h_range=peak_region)
+                                         sky_flux=avg_sky_flux, c_range=low_region, h_range=high_region)
             plt.savefig(output_dir / (fits_file_name + ".png"), dpi=300)
             plt.close()
 
             # Diagnostics - plot the spectra from every fibre (to a different file to the other plots - it's big!!)
             if plot_rss_spectra:
                 plotting.plot_rss_spectra(non_ss_spectra, flux_ratios, fits_file_name,
-                                          sky_mask, obj_mask, cont_region, peak_region, output_dir, expand_rss_spectra)
+                                          sky_mask, obj_mask, low_region, high_region, output_dir, expand_rss_spectra)
 
             fits_group_ss_spectra.append(ss_spectrum)
 
@@ -156,11 +160,14 @@ for spec_set in ["target_observation-red", "target_observation-blue", "standard_
                                title=f"My pipeline sky subtracted and combined spectrum for observations {fits_group}")
 
         # Save to a new fits file
-        image_data = np.fromfile(image_file_name)
-        new_file_name = output_dir / f"ss_{fits_group}.fits"
-        prim_hdu = fits.PrimaryHDU(image_data)
-        css_hdu = grp_spectrum.create_image_hdu("SPEC_CSS", header1)
+        # Copy the primary HDU from the first file of the set
+        data1, header1 = fits.getdata(source_fits_full_name1, hdu=0, header=True)
+        prim_hdu = fits.PrimaryHDU(data=data1, header=header1)
+
+        # Now create the HDU for the combined, sky subtracted spectrum - with the header based on that from source fits
+        css_hdu = grp_spectrum.create_image_hdu("SPEC_CSS", spec_ss_header)
+
         hdul = fits.HDUList([prim_hdu, css_hdu])
-        hdul.writeto(new_file_name, overwrite=True)
+        hdul.writeto(output_dir / f"ss_{fits_group}.fits", overwrite=True)
 
 
