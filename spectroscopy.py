f1b0c5d941f54b2da87e73450e1f43d7e74063b2
steveo <steveoverall@hotmail.co.uk>
2020-08-10 16:36:27 +0100

    Updated to work with a pair of sky-subtracted + flux-calibrate spectra that have been through my pipeline.  Proves these assets are readable/usable.

--- spectroscopy.py
+++ spectroscopy.py
@@ -8,22 +8,22 @@ from plot import *
 
 # Test code - will be replaced with a specific data source once spectral pipeline completed.
 spec_be, header = FrodoSpecSpectralDataSource.read_spectrum(
-    "/Users/steveo/Documents/V3890_Sgr_Data/LT_Spectra/JL19A06private2105703012reduced/SPEC_SS_extract/03.66_b_e_20190831_11_1_1_2_med_spec_ss.fits",
-    "SPEC_SS_MED", header=True)
+    "/Users/steveo/Documents/V3890_Sgr_Data/LT_Spectra/flux_cal_pipeline/calibrated/cal_b_e_20190828_3.fits",
+    "PRIMARY", header=True)
 
 spec_re = FrodoSpecSpectralDataSource.read_spectrum(
-    "/Users/steveo/Documents/V3890_Sgr_Data/LT_Spectra/JL19A06private2105703012reduced/SPEC_SS_extract/03.66_r_e_20190831_11_1_1_2_med_spec_ss.fits",
-    "SPEC_SS_MED")
+    "/Users/steveo/Documents/V3890_Sgr_Data/LT_Spectra/flux_cal_pipeline/calibrated/cal_r_e_20190828_3.fits",
+    "PRIMARY")
 
 fig = plt.figure(figsize=[6.4, 3.2], constrained_layout=True)
 ax = fig.add_subplot(1, 1, 1)
-ax.set_title(f"The blue and red arm spectra at $\\Delta t={header['DELTA-T']}$ d")
+ax.set_title(f"The sky-subtracted and flux calibrated spectra at MJD={header['MJD']:.2f} d")
 ax.set_xlabel(f"Wavelength [{spec_be.spectral_axis.unit}]")
 ax.set_ylabel(f"Arbitrary flux [{spec_be.flux.unit}]")
 ax.set_yticks([], minor=False)
 ax.set_xticks(np.arange(3500, 8500, 500), minor=False)
 ax.set_xticks(np.arange(3500, 8500, 50), minor=True)
-ax.plot(spec_be.spectral_axis, spec_be.flux, label=f"blue arm (gain={header['MED-GAIN']})", color="b", linestyle="-", linewidth=0.25)
+ax.plot(spec_be.spectral_axis, spec_be.flux, label=f"blue arm", color="b", linestyle="-", linewidth=0.25)
 ax.plot(spec_re.spectral_axis, spec_re.flux, label="red arm", color="r", linestyle="-", linewidth=0.25)
 ax.legend(loc="upper left")
 plt.show()
