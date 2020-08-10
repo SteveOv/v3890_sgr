cb5ed73a333df27ee84b3b6866e898e1508f7cc3
steveo <steveoverall@hotmail.co.uk>
2020-08-10 18:20:58 +0100

    Improvements to FrodoSpecSpectralDataSource
    * Reworked so that read_spectrum() no longer works by calling read_spectra() then selecting one.  Instead moved the code which prepares the wcs, flux and spectral_axis data into a common utility method which is called as needed by both read_spectrum() and read_spectra().  Both methods then load the data returned from this into a Spectrum1DEx and SpectrumCollectionEx respectively.
    
    * Improved behaviour when no HDU specified.  Now defaults to the 0th HDU - the primary.
    
    * Improved the labelling of SpectrumCollectionEx - now defaults to the HDU name if no label specified.

--- data/FrodoSpecSpectralDataSource.py
+++ data/FrodoSpecSpectralDataSource.py
@@ -15,7 +15,7 @@ class FrodoSpecSpectralDataSource(SpectralDataSource, ABC):
     """
 
     @classmethod
-    def read_spectra(cls, filename: Union[str, Path], hdu_name: str, selected_fibres: [] = None,
+    def read_spectra(cls, filename: Union[str, Path], hdu_name: str = None, selected_fibres: [] = None,
                      header: bool = False, label: str = None) \
             -> Union[SpectrumCollection, Tuple[Any, SpectrumCollectionEx]]:
         """
@@ -23,47 +23,21 @@ class FrodoSpecSpectralDataSource(SpectralDataSource, ABC):
         only those spectra where the mask is true are returned, otherwise all spectra are returned.
         Returns the SpectrumCollectionEx and optionally the HDU header.
         """
-        data, hdr = fits.getdata(filename, hdu_name, header=True)
-        hdr["CUNIT1"] = "Angstrom"          # It's actually "Angstroms" in the files
-
-        # Check the data and wavelength shapes match NAXIS1/2.  Need to get this right otherwise creating the
-        # wavelength/spectral_axis via the WCS or assigning it to SpecCollection fails (data/wavelengths must match)
-        # For FRODO SPEC_SS:    NAXIS1 = #flux_readings, NAXIS2 = 1                     data={ndarray: (1, xxxx)}
-        # For FRODO RSS_NONSS:  NAXIS1 = #flux_readings, NAXIS2 = fibre count (144)     data={ndarray: (144, xxxx)}
-        # For my early fits:    NAXIS1 = #flux_reasings, NAXIS2 = <missing>             data={ndarray: (xxxx,)}
-        # For my SPEC_SS_MED:   NAXIS1 = #flux_readings, NAXIS2 = 1                     data={ndarray: (1, xxxx)}
-        # Need to make sure we can cope with all of these! NAXIS1 = #flux/wavelength readings, NAXIS2 = #fibres/spectra
-        n_axis1 = hdr["NAXIS1"]
-        n_axis2 = hdr["NAXIS2"] if "NAXIS2" in hdr else None
-        if n_axis2 is None and len(data.shape) == 1:
-            # For some data written out NAXIS2 is missing and the data is in the form {ndarray: (xxxx,)}.
-            # This can be made usable if we interpret NAXIS2 = 1 and reshape the data to match; {ndarray: (1, xxxx)}.
-            hdr["NAXIS"] = 2
-            n_axis2 = hdr["NAXIS2"] = 1
-            data = data[np.newaxis, :]
-
-        # This decodes the headers which describe the spectral data
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            wcs = WCS(hdr)
-
-        # Need to supply it with a spectral_axis it can work with - has to be the right shape and size, as described by
-        # NAXIS1 x NAXIS2. From this the wcs (world coordinate system) can generate the wavelength axis. Even then, it's
-        # not right for specutils and we have to transpose it to match the data before setting as the spectral axis!
-        axis1 = np.arange(n_axis1)[:, np.newaxis]
-        axis2 = np.arange(n_axis2)[np.newaxis, :]
-        spectral_axis = (wcs.wcs_pix2world(axis1, axis2, 0)[0]).transpose()
-
-        # Now we should have both the data and wavelength data set up with the same size & shape.  Get them ready.
-        assert data.shape == spectral_axis.shape
+        hdr, flux, spectral_axis, wcs = cls._read_data_from_fits(filename, hdu_name)
         if selected_fibres is None:
-            flux = data * units.Unit("adu")
+            flux = flux * units.Unit("adu")
             spectral_axis = spectral_axis * units.Unit("Angstrom")
         else:
-            selection_mask = np.zeros(data.shape[0], dtype=bool)
+            selection_mask = np.zeros(flux.shape[0], dtype=bool)
             selection_mask.put(selected_fibres, True)
-            flux = data[selected_fibres] * units.Unit("adu")
+            flux = flux[selected_fibres] * units.Unit("adu")
             spectral_axis = spectral_axis[selected_fibres] * units.Unit("Angstrom")
+
+        # Default the label to the HDU name
+        if label is None:
+            if hdu_name is None:
+                hdu_name = hdr["EXTNAME"] if "EXTNAME" in hdr else "unnamed"
+            label = hdu_name
 
         spectra = SpectrumCollectionEx(flux=flux, spectral_axis=spectral_axis, wcs=wcs, label=label)
         if header:
@@ -72,16 +46,71 @@ class FrodoSpecSpectralDataSource(SpectralDataSource, ABC):
             return spectra
 
     @classmethod
-    def read_spectrum(cls, filename: Union[str, Path], hdu_name: str, fibre: int = 0, header: bool = False) \
+    def read_spectrum(cls, filename: Union[str, Path], hdu_name: str = None, index: int = 0, header: bool = False) \
             -> Union[Spectrum1D, Tuple[Spectrum1D, Any]]:
         """
-        Read the requested spectral data into a specutils Spectrum1D.  The fibre is used to specify
+        Read the requested spectral data into a specutils Spectrum1D.  The index is used to specify
         which member to return if the HDU contains multiple spectra (defaults to zero).
         Returns the Spectrum1D and optionally the HDU header.
         """
-        spectra, hdr = cls.read_spectra(filename, hdu_name, [fibre], header=True)
-        spectrum = spectra[0, :]
+        hdr, flux, spectral_axis, wcs = cls._read_data_from_fits(filename, hdu_name)
+        flux = flux[index] * units.Unit("adu")
+        spectral_axis = spectral_axis[index] * units.Unit("Angstrom")
+
+        spectrum = Spectrum1DEx(flux=flux, spectral_axis=spectral_axis, wcs=wcs)
         if header:
             return spectrum, hdr
         else:
             return spectrum
+
+    @classmethod
+    def _read_data_from_fits(cls, filename: Union[str, Path], hdu_name: str = None):
+        """
+        This will read in the spectral data and header from the requested fits file
+        then parse and  prepare it for loading into specutils objects.
+
+        :returns
+            header - the HDU headers
+            flux - the 2d ndarray containing the flux data
+            spectral_axis - the 2d ndarray containing the complementary spectral/wavelength axis data.
+            wcs - the WCS used when parsing the data
+        """
+
+        if hdu_name is not None:
+            flux, hdr = fits.getdata(filename, extname=hdu_name, header=True)
+        else:
+            flux, hdr = fits.getdata(filename, ext=0, header=True)
+
+        hdr["CUNIT1"] = "Angstrom"          # It's actually "Angstroms" in the files
+
+        # Check the data and wavelength shapes match NAXIS1/2.  Need to get this right otherwise creating the
+        # wavelength/spectral_axis via the WCS or assigning it to specutils object fails (data/wavelengths must match)
+        # For FRODO SPEC_SS:    NAXIS1 = #flux_readings, NAXIS2 = 1                     data={ndarray: (1, xxxx)}
+        # For FRODO RSS_NONSS:  NAXIS1 = #flux_readings, NAXIS2 = fibre count (144)     data={ndarray: (144, xxxx)}
+        # For my early fits:    NAXIS1 = #flux_reasings, NAXIS2 = <missing>             data={ndarray: (xxxx,)}
+        # For my SPEC_SS_MED:   NAXIS1 = #flux_readings, NAXIS2 = 1                     data={ndarray: (1, xxxx)}
+        # Need to make sure we can cope with all of these! NAXIS1 = #flux/wavelength readings, NAXIS2 = #fibres/spectra
+        n_axis1 = hdr["NAXIS1"]
+        n_axis2 = hdr["NAXIS2"] if "NAXIS2" in hdr else None
+        if n_axis2 is None and len(flux.shape) == 1:
+            # For some data written out NAXIS2 is missing and the data is in the form {ndarray: (xxxx,)}.
+            # This can be made usable if we interpret NAXIS2 = 1 and reshape the data to match; {ndarray: (1, xxxx)}.
+            hdr["NAXIS"] = 2
+            n_axis2 = hdr["NAXIS2"] = 1
+            flux = flux[np.newaxis, :]
+
+        # This decodes the headers which describe the spectral data
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            wcs = WCS(hdr)
+
+        # Need to supply it with a spectral_axis it can work with - has to be the right shape and size as described by
+        # NAXIS1 x NAXIS2. From this the wcs (world coordinate system) can generate the wavelength axis. Even then, it's
+        # not right for specutils and we have to transpose it to match the data before setting as the spectral axis!
+        axis1 = np.arange(n_axis1)[:, np.newaxis]
+        axis2 = np.arange(n_axis2)[np.newaxis, :]
+        spectral_axis = (wcs.wcs_pix2world(axis1, axis2, 0)[0]).transpose()
+
+        # Now we should have both the data and wavelength data set up with the same size & shape.  Get them ready.
+        assert flux.shape == spectral_axis.shape
+        return hdr, flux, spectral_axis, wcs
