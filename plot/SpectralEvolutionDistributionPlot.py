f963450faa841a8c7a3a2e5c4991d4ea047119df
steveo <steveoverall@hotmail.co.uk>
2020-08-12 21:17:36 +0100

    Work on the SED plot
    * Reworked how the flux and subsequent luminosities are calculated so that we also cover the UVOT UVM2 and UVW2 bands.  Now based on calculating the flux from the magnitude through the relation
           m_AB = -2.5 log(f_nu) + 8.9
      Assuming we have Vega fluxes (to be confirmed), there is a correction factor which is applied to get m_AB for each of the bands before the flux is calculated from that.
    
    * Removed the distance uncertainty from the luminosity calculations.  It's a systematic, so instead an extra point for the "Distance uncertainty" is plotted near the bottom of the Axes.
    
    * Just some general tidying up to make things a bit more readable.
    
    * Still to do
    - Extinction correction
    - 2nd y-axis showing magnitudes
    - colouring the plots to differentiate different periods in the evolution (a la Darnley et al., 2016)

--- plot/SpectralEvolutionDistributionPlot.py
+++ plot/SpectralEvolutionDistributionPlot.py
@@ -1,11 +1,22 @@
 import math
-from data import MagnitudeDataSource
 from uncertainties import ufloat, ufloat_fromstr, UFloat
 import with_uncertainties as unc
 from plot.TimePlotSupportingLogAxes import *
 
 
 class SpectralEvolutionDistributionPlot(TimePlotSupportingLogAxes):
+
+    _mag_ab_correction_factor = {
+        # FROM LJMU Website
+        "B": -0.1,
+        "V": 0,
+        "R": 0.2,
+        "I": 0.45,
+
+        # From Breeveld 2010, Swift-UVOT-CALDDB-16-R01
+        "UVM2": 1.69,
+        "UVW2": 1.73
+    }
 
     def __init__(self, plot_params: Dict):
         super().__init__(plot_params, x_axis_supports_log=True, y_axis_supports_log=True)
@@ -18,7 +29,7 @@ class SpectralEvolutionDistributionPlot(TimePlotSupportingLogAxes):
         # log(luminosity) scale derived from the flux density and distance.
         self._default_y_label = "$\\log(L_{\\nu})$ [Jy m$^2$]"
         # super() doesn't restrict the y-axis, so we implement these properties here
-        self._default_y_lim_log = (5e38, 3e43)
+        self._default_y_lim_log = (1e38, 3e43)
         self._default_y_ticks_log = [1e39, 1e40, 1e41, 1e42, 1e43]
         self._default_y_tick_labels_log = ["39", "40", "41", "42", "43"]
 
@@ -104,8 +115,13 @@ class SpectralEvolutionDistributionPlot(TimePlotSupportingLogAxes):
         nu_effs = self._param(
             "nu_eff", {"I": 3.72e14, "R": 4.556e14, "V": 5.441e14, "B": 6.737e14, "UVM2": 1.335e15, "UVW2": 1.555e15})
 
+        r_m, r_m_err = unc.multiply(self.target_distance_parsecs.nominal_value, 3.086e16,
+                                    self.target_distance_parsecs.std_dev, 0)
+
         # This is an interim step - parses the source data and calculates flux & luminosity fields
-        df = self._perform_sed_analysis_fluxes(plot_sets, nu_effs, delta_ts)
+        # We don't include the distance uncertainty in the calculations as it's systematic.
+        # Instead we'll work it out once and present it separately.
+        df = self.__class__._calculate_sed_data(plot_sets, nu_effs, delta_ts, r_m, distance_m_err=0)
 
         ix = 0
         for delta_t in sorted(delta_ts):
@@ -119,7 +135,7 @@ class SpectralEvolutionDistributionPlot(TimePlotSupportingLogAxes):
             y_pos = last_good_band["L_nu"]
             # TODO: specific to this plot, handle a crush
             if ix == 5:
-                y_pos += 0.2
+                y_pos -= 3e39
 
             # Annotate the Delta t at the right end of each line
             label = f"$\\Delta t={delta_t:.2f}$" if delta_t != int(delta_t) else f"$\\Delta t={delta_t}$"
@@ -131,10 +147,42 @@ class SpectralEvolutionDistributionPlot(TimePlotSupportingLogAxes):
         for band in nu_effs:
             ax.annotate(band, xycoords="data", xy=(nu_effs[band], y_pos), horizontalalignment="center")
 
+        # Add a single point, but with the distance uncertainty so that we have a representation
+        # of the systematic distance error.  Get a flux density for a luminosity of 10^39.
+        # Then put it back into the luminosity calculation, but with the distance uncertainty and plot the result.
+        x_pos = nu_effs["I"]
+        y_pos = 2e38
+        f_nu, f_nu_err = unc.divide(y_pos, 4 * math.pi * np.power(r_m, 2))
+        lum_nu, lum_nu_err = \
+            self.__class__._calculate_specific_luminosity_for_flux_and_distance(f_nu, f_nu_err, r_m, r_m_err)
+        self._plot_points_to_error_bars_on_ax(ax, [x_pos], [lum_nu], [lum_nu_err], "k", fmt=",")
+        ax.annotate("Distance uncertainty", xycoords="data", xy=(x_pos + 10**13, y_pos - 2e37))
         return
 
-    def _perform_sed_analysis_magnitudes(self, plot_sets: Dict[str, PlotSet],
-                                         nu_eff_lookup: Dict, delta_ts: Dict) -> DataFrame:
+    @classmethod
+    def _calculate_sed_data(cls, plot_sets: Dict[str, PlotSet],
+                            nu_eff_lookup: Dict, delta_ts: Dict,
+                            distance_m: float, distance_m_err: float=0) -> DataFrame:
+
+        df = cls._get_magnitudes_from_photometric_fits(plot_sets, nu_eff_lookup, delta_ts)
+
+        # TODO: extinction correction.
+
+        # Calculate flux density values for the retrieved data.
+        df[['flux_hz', "flux_hz_err"]] = df.apply(
+            lambda d: cls._calculate_flux_density_for_magnitude_and_band(d["mag"], d["mag_err"], d["band"]),
+            axis=1,
+            result_type="expand")
+
+        # Calculate the specific luminosity from the flux density
+        df[["L_nu", "L_nu_err"]] = df.apply(
+            lambda d: cls._calculate_specific_luminosity_for_flux_and_distance(
+                d["flux_hz"], d["flux_hz_err"], distance_m, distance_m_err), axis=1, result_type="expand")
+        return df
+
+    @classmethod
+    def _get_magnitudes_from_photometric_fits(cls, plot_sets: Dict[str, PlotSet],
+                                              nu_eff_lookup: Dict, delta_ts: Dict) -> DataFrame:
         rows = []
         for plot_set_key in plot_sets:
             plot_set = plot_sets[plot_set_key]
@@ -154,32 +202,31 @@ class SpectralEvolutionDistributionPlot(TimePlotSupportingLogAxes):
             df = None
         return df
 
-    def _perform_sed_analysis_fluxes(self, plot_sets: Dict[str, PlotSet],
-                                     nu_eff_lookup: Dict, delta_ts: Dict) -> DataFrame:
-        df = self._perform_sed_analysis_magnitudes(plot_sets, nu_eff_lookup, delta_ts)
-
-        # If not already present, calculate flux density values for the retrieved data.
-        df[['flux_hz', "flux_hz_err"]] = df.apply(
-            lambda d: MagnitudeDataSource.flux_density_from_magnitude(d["mag"], d["mag_err"], d["band"]),
-            axis=1,
-            result_type="expand")
-
-        # Calculate the specific luminosity from the flux density
-        df[["L_nu", "L_nu_err"]] = df.apply(
-            lambda d: self._calculate_specific_luminosity(d["flux_hz"], d["flux_hz_err"]), axis=1, result_type="expand")
-        return df
-
-    def _calculate_specific_luminosity(self, flux, flux_err):
+    @classmethod
+    def _calculate_flux_density_for_magnitude_and_band(cls, mag, mag_err, band):
         """
-        Calculate the specific luminosity based on the passed flux density and distance
+        Calculate the flux density from the passed magnitude.
+        """
+        #
+        # f_nu = 10 ^ 0.4(8.9 - m_AB)
+        #
+        # Based on the relation: m_AB = -2.5 log(f_nu) + 8.9
+        # https://www.astro.ljmu.ac.uk/~ikb/convert-units/node1.html
+        #
+        m_ab, m_ab_err = unc.add(mag, cls._mag_ab_correction_factor[band], mag_err, 0)
+        m_int, m_int_err = unc.subtract(8.9, m_ab, 0, m_ab_err)
+        exponent, exponent_err = unc.multiply(0.4, m_int, 0, m_int_err)
+        f_nu, f_nu_err = unc.power(10, exponent, 0, exponent_err)
+        return f_nu, f_nu_err
+
+    @classmethod
+    def _calculate_specific_luminosity_for_flux_and_distance(cls, flux, flux_err, distance_m, distance_m_err=0):
+        """
+        Calculate the specific luminosity based on the passed flux density and distance (in metres)
         using the relation: L_nu = 4pi * r^2 * F_nu
         """
-        # Convert the configured distance from pc to m
-        r, r_err = unc.multiply(self.target_distance_parsecs.nominal_value, 3.086e16,
-                                self.target_distance_parsecs.std_dev, 0)
-
         # Calculate the luminosity L_nu = 4pi * r^2 * F_nu
-        a, a_err = unc.multiply(flux, 4*math.pi, flux_err)
-        r2, r2_err = unc.power(r, 2, r_err)
+        a, a_err = unc.multiply(flux, 4*math.pi, flux_err, 0)
+        r2, r2_err = unc.power(distance_m, 2, distance_m_err, 0)
         l_nu, l_nu_err = unc.multiply(a, r2, a_err, r2_err)
         return l_nu, l_nu_err
