Fixes the calculation of E(B-V) from E(g-r) values (there was a schoolboy error in the original maths).
Also moved the code for this into the colors.py module and added 3 more algorithms to calculate this, as taken from the SDSS Dr13 documentations
* Jester et al. (2005) AJ, 130, 3
    * Stars where R_C-I_C < 1.15
* Jordi et al. (2006) A&A, 460
    * Population I stars
    * Population II stars

Finally, updated photometry.py to use the mean of the Lupton and the Jordi algorithms as the E(B-V) value for distance calculations and subsequent plotting (Color-Mag & SED use this).

photometry.py
@@ -121,8 +121,12 @@ for group_key in ['V3890-Sgr-2019-Vis-nominal-err', 'V3890-Sgr-2019-Vis-nominal+
     # Significantly lower than Schaefer's E(B-V) of 0.9 +/- 0.3.
     # As recommended by Matt Darnley (private communication), we use the Pan-STARRS reddening survey along
     # alpha=277.68, delta=-24.019 for r>5 kpc (which V380 Sgr has with even E(B-V)=0.9) giving E(g-r)=0.51 +/- 0.02
-    E_BV = ufloat(*mag.E_gr_to_E_BV(0.51, 0.02))
-    print(F"[{group_key}] Using E(B-V) = {E_BV:.4f} (derived from Pan-STARRS E(g-r) = 0.51 +/- 0.02)")
+    E_BVs = [
+        ufloat(*colors.E_BV_from_E_gr_Jordi_metal_poor(0.514, 0.002)),
+        ufloat(*colors.E_BV_from_E_gr_Jordi_populationI(0.514, 0.002)),
+        ufloat(*colors.E_BV_from_E_gr_Lupton(0.514, 0.002))]
+    E_BV = np.mean(E_BVs)
+    print(F"[{group_key}] Using E(B-V) = {E_BV:.3f} (derived from Pan-STARRS E(g-r) = 0.514 +/- 0.002)")
 
     # Now use the MMRD to work out the absolute magnitude
     M_V_tp = rn.absolute_magnitude_from_t2_fast_nova(t2)